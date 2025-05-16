c0787e9c51db9da7548a45e83029c42daf718550
Stevie Robinson <stevie.robinson@gmail.com>
2025-05-16 19:13:10 +0200

    update helper to allow for empty id or class

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,7 +1,12 @@
 export function Element(tag, classList = [], id ='', content = '') {
     let element = document.createElement(tag);
-    element.classList.add(...classList);
-    element.id = id.split(' ').join('');
+
+    if (classList.length > 0) {
+        element.classList.add(...classList);
+    }
+    
+    id ? element.id = id.split(' ').join('') : null;
+    
     element.textContent = content;
 
     return element;
